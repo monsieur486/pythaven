7c61e9e81e0b4c34503363a2b4faaad7d98146e3
monsieur486 <monsieur486@gmail.com>
2022-01-26 13:38:05 +0100

    First commit.

--- gestoven/settings.py
+++ gestoven/settings.py
@@ -10,13 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-94kv5nb*wasb*-rj5-#81i_0-bvgf_qppw&%*^jyztypgubyck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -86,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
--- gestoven/urls.py
+++ gestoven/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin/', admin.site.urls),
 ]
