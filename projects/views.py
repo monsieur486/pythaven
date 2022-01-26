7627d1507f838668274884aaf21c5a671a926231
monsieur486 <monsieur486@gmail.com>
2022-01-26 14:35:29 +0100

    First commit.

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
