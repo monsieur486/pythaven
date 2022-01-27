0cf30e25c931ef86e573947c6a407b2bb30ea8da
monsieur486 <monsieur486@gmail.com>
2022-01-27 01:26:05 +0100

    First commit.

--- gestoven/settings.py
+++ gestoven/settings.py
@@ -64,10 +64,8 @@ WSGI_APPLICATION = 'gestoven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'bd_config.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
